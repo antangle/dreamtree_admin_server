7bea528be1c88c617ff36e75fa6acdbfc0128f2f
antangle <zanaris@naver.com>
2023-02-13 14:19:47 +0900

    파일 구조 세팅

---
+++ src/main/java/com/dreamtree/api/common/dto/PageRequestDTO.java
@@ -0,0 +1,52 @@
+package com.dreamtree.api.common.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageRequestDTO {
+
+    @Builder.Default
+    private int page = 1;
+
+    @Builder.Default
+    private int size = 10;
+
+
+//    private String link;
+//
+//    public String getLink() {
+//
+//        if (link == null) {
+//            StringBuilder builder = new StringBuilder();
+//
+//            builder.append("page=" + this.page);
+//
+//            builder.append("&size=" + this.size);
+//
+//
+//            if (type != null && type.length() > 0) {
+//                builder.append("&type=" + type);
+//            }
+//
+//            if (keyword != null) {
+//                try {
+//                    builder.append("&keyword=" + URLEncoder.encode(keyword, "UTF-8"));
+//                } catch (UnsupportedEncodingException e) {
+//                }
+//            }
+//            link = builder.toString();
+//        }
+//
+//        return link;
+//    }
+
+    public int getSkip() {
+
+        return (page - 1) * size;
+    }
+}
