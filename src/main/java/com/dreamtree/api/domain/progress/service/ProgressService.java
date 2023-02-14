e07ad5d88d1bb69fa492b0e3465ba0060e2a4168
syeon-c <gkdltjdus@gmail.com>
2023-02-14 15:51:07 +0900

    progress domain...parent branch/ parent_development branch merge

--- src/main/java/com/dreamtree/api/domain/progress/service/ProgressService.java
+++ src/main/java/com/dreamtree/api/domain/progress/service/ProgressService.java
@@ -1,4 +1,11 @@
 package com.dreamtree.api.domain.progress.service;
 
+import com.dreamtree.api.domain.progress.dto.ProgressResDTO;
+
+import java.util.List;
+
 public interface ProgressService {
+
+    List<ProgressResDTO> getProgressList(Long id);
+
 }
